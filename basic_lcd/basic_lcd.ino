f08f58b3c541eddf7bbceee1366a1b22b65d8a8f
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-04-09 17:16:17 -0400

    Read analog values from buttons and display

--- basic_lcd/basic_lcd.ino
+++ basic_lcd/basic_lcd.ino
@@ -1,20 +1,28 @@
 #include <LiquidCrystal.h>
 
 const uint8_t rs = 8, enable = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-
+uint16_t button = 0;
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
 void setup()
 {
   // 16 cols, 2 rows
   lcd.begin(16, 2);
-
+  // Start at col, 0 row 0
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
 }
 
 void loop()
 {
-  // Start at col, 0 row 0
-  lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
+  lcd.setCursor(0, 1);
+  button = analogRead(0);
+  // Print blanks to clear screen
+  lcd.print("    ");
+  // Go back to beginning of line
+  lcd.setCursor(0, 1);
+  // Print the analog reading
+  lcd.print(button);
+  delay(100);
 
 }
