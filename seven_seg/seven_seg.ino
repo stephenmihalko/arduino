c2ab003434ac1ec9a52a12af4b2cda0bc34ef6ee
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-04-11 09:58:01 -0400

    Add hexadecimal digits to scroll

--- seven_seg/seven_seg.ino
+++ seven_seg/seven_seg.ino
@@ -9,8 +9,14 @@
  * g (M):  7
  */
 
-// Bit masks for numbers 0-9: the index is the number
-const uint8_t masks[10] = {0x7E, 0x0C, 0xB6, 0x9E, 0xCC, 0xDA, 0xFA, 0x0E, 0xFE, 0xDE};
+/* Bit masks for numbers 0-F: the index is the number
+ * - Pin 0 (LSB) should always be zero
+ * - g is the MSB
+ */
+const uint8_t masks[16] = {0x7E, 0x0C, 0xB6, 0x9E,
+						   0xCC, 0xDA, 0xFA, 0x0E,
+						   0xFE, 0xDE, 0xEE, 0xF8,
+						   0x71, 0xCB, 0xF2, 0xE2};
 void setup()
 {
   // Set pins 1-7 to output
@@ -19,10 +25,10 @@ void setup()
 
 void loop()
 {
-  for (size_t i = 0; i < 10; ++i)
+  for (size_t i = 0; i < 16; ++i)
   {
-    // Clear all LEDs
-    PORTD &= 0x00;
+    // Clear all LEDs, but leave pin 0 alone
+    PORTD &= 0x01;
     // Send number to seven-segment display
     PORTD |= masks[i];
     delay(250);
