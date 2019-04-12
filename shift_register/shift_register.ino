18d15dc296a1864c4744d0c6e30cee7677a810f3
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-04-11 20:29:26 -0400

    Complete setup function and begin pin mapping

--- shift_register/shift_register.ino
+++ shift_register/shift_register.ino
@@ -1,6 +1,18 @@
+const uint8_t data = 12, clk = 13;
+
+/*
+ * Bit layout
+ * REGISTER: 7-SEG
+ *       Qh: dp  <-- MSB
+ *       Qk: k
+ */
+
+const uint8_t masks[16] = { };
+
 void setup()
 {
-  
+  // Set pins 12 and 13 to output
+  DDRB |= 0x30
 }
 
 void loop()
