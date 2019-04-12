6f3197631d50cb05d3d963e937f30fb75a2e0426
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-04-11 20:37:32 -0400

    Cycle 7-seg display through 0-3

--- shift_register/shift_register.ino
+++ shift_register/shift_register.ino
@@ -1,4 +1,6 @@
-const uint8_t data = 12, clk = 13;
+const uint8_t data = 12,  // the data pin in Arduino
+              clk = 13,   // the clock pin in Arduino
+              dp = 0x80;  // OR number bitmask with this for dp
 
 /*
  * Bit layout
@@ -7,15 +9,19 @@ const uint8_t data = 12, clk = 13;
  *       Qk: k
  */
 
-const uint8_t masks[16] = { };
+const uint8_t masks[4] = {0x3F, 0x06, 0x5B, 0x4F};
 
 void setup()
 {
   // Set pins 12 and 13 to output
-  DDRB |= 0x30
+  DDRB |= 0x30;
 }
 
 void loop()
 {
-  
+  for (size_t i = 0; i < 4; ++i)
+  {
+    shiftOut(data, clk, MSBFIRST, masks[i]);
+    delay(1000);
+  }
 }
